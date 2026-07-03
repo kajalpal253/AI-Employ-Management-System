cc5e221a9a933055aa7854f8f3b907105555117b
kajal <kpal53249@gmail.com>
2026-07-03 10:49:52 +0530

    fix core

--- src/main/java/com/management/employee/security/SecurityConfig.java
+++ src/main/java/com/management/employee/security/SecurityConfig.java
@@ -49,12 +49,20 @@ public class SecurityConfig {
 
     @Bean
 public CorsConfigurationSource corsConfigurationSource() {
+
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(List.of("http://localhost:3000","https://ai-employee-management-system-front-gray.vercel.app"));
-    configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
-        configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowedOrigins(List.of(
+        "http://localhost:3000",
+        "https://ai-employee-management-system-front-gray.vercel.app"
+    ));
 
+    configuration.setAllowedMethods(List.of(
+        "GET", "POST", "PUT", "DELETE", "OPTIONS"
+    ));
+
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source =
             new UrlBasedCorsConfigurationSource();
